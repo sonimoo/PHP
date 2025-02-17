77d311d4c33b4da29dc97345a20c207de1dcbae0
sonimoo <145650788+sonimoo@users.noreply.github.com>
2025-02-17 12:38:43 +0200

    Add files via upload

---
+++ php_lab01/index.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Привет, мир!<br>";
+echo "Hello, World with echo!<br>";
+print "Hello, World with print!<br>";
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+echo $days . " days before Xday.<br>";
+echo "Праздники закончились!!! {$message}<br>";
+echo "Праздники закончились!!! $message<br>";
