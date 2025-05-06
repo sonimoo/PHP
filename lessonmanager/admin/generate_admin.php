f3d103e9fd30ac82cdc2fba442ae0dc3caa545f7
sonimoo <145650788+sonimoo@users.noreply.github.com>
2025-05-06 22:47:18 +0300

    oi oi

--- lessonmanager/admin/generate_admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Генерация хеша пароля для администратора
-$password = 'admin'; // Это пароль администратора
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-echo $hashed_password; // Выведет хеш пароля, который нужно вставить в базу данных
-?>
